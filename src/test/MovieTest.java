448e65a4d588b15785bf0412ab37cc7028e4184e
Waldemar Merk <merk.waldemar@gmail.com>
2017-05-12 19:58:42 +0200

    added movie tests

---
+++ src/test/MovieTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Merk Waldemar on 12.05.2017.
+ */
+class MovieTest {
+    @Test
+    void setPriceCode() {
+        String title = "Test";
+        int priceCode = 1;
+        Movie movie = new Movie(title,priceCode);
+
+        movie.setPriceCode(Movie.NEW_RELEASE);
+        Assertions.assertEquals( PriceNewRelease.class , movie.price.getClass());
+
+        movie.setPriceCode(Movie.CHILDRENS);
+        Assertions.assertEquals( PriceChildrens.class , movie.price.getClass());
+
+        movie.setPriceCode(Movie.REGULAR);
+        Assertions.assertEquals( PriceRegular.class , movie.price.getClass());
+    }
+
+    @Test
+    void getTitle() {
+        String title = "Test";
+        int priceCode = 2;
+        Movie movie = new Movie(title,priceCode);
+
+        Assertions.assertEquals( title , movie.getTitle());
+    }
+
+}
